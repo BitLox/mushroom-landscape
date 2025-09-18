2f77135135be721e2f67a7e8c2d965bfa5342533
Dana Coe <dana.coe@bitlox.com>
2025-09-18 15:27:38 +0800

    Treaked title text animation

--- script.js
+++ script.js
@@ -50,7 +50,8 @@ const cloudDriftR = gsap.to("#cloudStart-R", {
 // Color-cycling timeline (unchanged, non-scroll-based)
 const tl = gsap.timeline({ repeat: -1, yoyo: true });
 const colors = ['#ff0000', '#00ff00', '#0000ff', '#ff00ff', '#00ffff', '#ff4500', '#9932cc', '#00ced1', '#ff69b4', '#4682b4'];
-const selectors = ['#text1', '#text2top', '#text3', '#text4', '#text5', '#text6', '#text7', '#text8', '#text9', '#text10', '#text11', '#text12', '#text13', '#scene2_text', '#scene2_text_a', '#scene2_text_b', '#scene2_text_c','#testing2', '#scene3_text'];
+// const selectors = ['#text1', '#text2top', '#text3', '#text4', '#text5', '#text6', '#text7', '#text8', '#text9', '#text10', '#text11', '#text12', '#text13', '#scene2_text', '#scene2_text_a', '#scene2_text_b', '#scene2_text_c','#testing2', '#scene3_text'];
+const selectors = ['#info', '#scene2_text', '#scene2_text_a', '#scene2_text_b', '#scene2_text_c','#testing2', '#scene3_text'];
 selectors.forEach((selector, index) => {
   const shiftedColors = [...colors.slice(index), ...colors.slice(0, index)];
   shiftedColors.forEach((color, colorIndex) => {
